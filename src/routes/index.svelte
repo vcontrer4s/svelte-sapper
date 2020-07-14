36fec6a9b57b60fc9d0664da7f634e50c322a7c0
vcontrer4s <vikmacon@gmail.com>
2020-07-14 10:16:49 -0400

    curso

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+
+  let data = {};
+  const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
+
+  onMount(async () => {
+    const response = await fetch(API);
+    data = await response.json();
+  });
+</script>
+
+<Main>
+  <TimeLine posts={data.posts} />
+  <Sidebar {...data.user} />
+</Main>
